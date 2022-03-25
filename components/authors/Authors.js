243d184100f7130ed45fcadc1c67a0243b1ef84b
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-03-25 19:29:41 +0600

    fetching data

--- components/authors/Authors.js
+++ components/authors/Authors.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import ListItemComponent from "./ListItemComponent";
 
 const Authors = () => {
-    return (
-        <div className="text-center text-3xl">
-            this is author page
-            
-        </div>
-    );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://api.quotable.io/authors?skip=20")
+      .then((res) => {
+        setData(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 gap-4 text-center text-3xl">
+      {data.map((result) => (
+        <ListItemComponent key={result._id} result={result}></ListItemComponent>
+      ))}
+    </div>
+  );
 };
 
 export default Authors;
