28edd1ce6b3e5c4e732eb4e45f0aae9bdf1a084e
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-03-26 22:10:05 +0600

    remove usestate

--- components/authors/ListItemComponent.js
+++ components/authors/ListItemComponent.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { UserFavourite } from "./../../pages/_app";
 
 const ListItemComponent = ({ result }) => {
