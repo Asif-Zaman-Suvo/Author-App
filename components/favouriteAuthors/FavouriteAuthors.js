a6627a3dfad64302c9c0ce7bd1a1d6dcaac55296
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-03-27 01:19:19 +0600

    remove state

--- components/favouriteAuthors/FavouriteAuthors.js
+++ components/favouriteAuthors/FavouriteAuthors.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { UserFavourite } from "../../pages/_app";
 import SingleFavouriteAuthor from "./SingleFavouriteAuthor";
 import { useRouter } from "next/router";
